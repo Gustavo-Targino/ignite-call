7b58a58e2f86c60a80a80656d47e6c7bd285ad4d
Gustavo Targino <gustavo.targinocc@hotmail.com>
2025-01-14 16:10:53 -0300

    feat: Estrutura do componente de calendário

---
+++ src/components/Calendar/styles.ts
@@ -0,0 +1,52 @@
+import { styled, Text } from "@ignite-ui/react";
+
+export const CalendarContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "$6",
+  padding: "$6",
+});
+
+export const CalendarHeader = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const CalendarTitle = styled(Text, {
+  fontWeight: "$medium",
+
+  span: {
+    color: "$gray200",
+  },
+});
+
+export const CalendarActions = styled("div", {
+  display: "flex",
+  gap: "$2",
+  color: "$gray200",
+
+  button: {
+    all: "unset",
+    cursor: "pointer",
+    lineHeight: 0,
+    borderRadius: "$sm",
+
+    svg: {
+      width: "$5",
+      height: "$5",
+    },
+
+    "&:hover": {
+      color: "gray",
+    },
+
+    "&:focus": {
+      boxShadow: "0 0 0 2px $colors$gray100",
+    },
+  },
+});
+
+export const CalendarBody = styled("table", {});
+
+export const CalendarDay = styled("button", {});
